e17460f19a6f8d20755eac1286b38ecced1ca168
Jeong Jineun <wlsdms0122@gmail.com>
2023-02-11 10:43:35 +0900

    feat(): add sugar functions

--- Sources/Route/UIViewController+Route.swift
+++ Sources/Route/UIViewController+Route.swift
@@ -46,15 +46,15 @@ extension UIViewController {
     }
     
     /// Route to a view controller. if it exist in receiver's children.
-    public func route(
-        _ viewControlelr: UIViewController,
+    public func route<ViewController: UIViewController>(
+        to viewControlelr: ViewController,
         animated: Bool,
-        completion: ((UIViewController?) -> Void)? = nil
+        completion: ((ViewController?) -> Void)? = nil
     ) {
         route(
             animated: animated,
             where: { $0 == viewControlelr },
-            completion: { completion?($0) }
+            completion: { completion?($0 as? ViewController) }
         )
     }
     
@@ -94,6 +94,16 @@ extension UIViewController {
         let subViewControllers = allViewControllers[ startIndex ..< endIndex]
         
         return subViewControllers.filter(predicate)
+    }
+    
+    public func search<ViewController: UIViewController>(of viewController: ViewController) -> ViewController? {
+        search { $0 == viewController }
+            .first as? ViewController
+    }
+    
+    public func search<ViewController>(of type: ViewController.Type) -> [ViewController] {
+        search { $0 is ViewController }
+            .compactMap { $0 as? ViewController }
     }
     
     public var allViewControllers: [UIViewController] {
